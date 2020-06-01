d425a505747e1f24db3ca2f6bf326b4718096be8
guisilvaf <guilherme@LAPTOP-2P56KC9M.localdomain>
2020-06-01 15:49:59 -0300

    Responsividade das páginas e adicionados dados nas telas de perfil e contrato.

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { CADASTROS } from "../listaCadastro";
+import { UsersService } from '../services/users.service';
 
 @Component({
-  selector: 'app-tab4',
-  templateUrl: './tab4.page.html',
-  styleUrls: ['./tab4.page.scss'],
+  selector: "app-tab4",
+  templateUrl: "tab4.page.html",
+  styleUrls: ["tab4.page.scss"]
 })
-export class Tab4Page implements OnInit {
+export class Tab4Page {
+  public workers = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private usersService: UsersService) {
+    this.getWorkers();
   }
 
+  getWorkers() {
+    this.usersService.getWorkers()
+    .subscribe(workers => {
+      this.workers = workers;
+    });
+  }
 }
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { CADASTROS } from "../listaCadastro";
+import { UsersService } from '../services/users.service';
 
 @Component({
-  selector: 'app-tab5',
-  templateUrl: './tab5.page.html',
-  styleUrls: ['./tab5.page.scss'],
+  selector: "app-tab5",
+  templateUrl: "tab5.page.html",
+  styleUrls: ["tab5.page.scss"]
 })
-export class Tab5Page implements OnInit {
+export class Tab5Page {
+  public workers = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private usersService: UsersService) {
+    this.getWorkers();
   }
 
+  getWorkers() {
+    this.usersService.getWorkers()
+    .subscribe(workers => {
+      this.workers = workers;
+    });
+  }
 }
