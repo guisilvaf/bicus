931499bcd7fe28ac20e4b2e9ea1bb07e66edd2b8
guisilvaf <guilherme@LAPTOP-2P56KC9M.localdomain>
2020-06-25 01:32:27 -0300

    Recuperação de senha e visualização de fotos de perfil.

---
+++ src/app/tab6/tab6.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CADASTROS } from "../listaCadastro";
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../services/users.service';
+
+@Component({
+  selector: 'app-tab6',
+  templateUrl: './tab6.page.html',
+  styleUrls: ['./tab6.page.scss'],
+})
+
+export class Tab6Page implements OnInit {
+	public userData ={
+		email: null,
+		senha: null
+	};
+	private workerCPF;
+	public workerData = [];
+	
+  
+  constructor(
+    private usersService: UsersService,
+    private route: ActivatedRoute
+  ) { 
+
+    this.route.params.subscribe(params => this.workerCPF = params.workerCPF);
+  }
+
+  ngOnInit() {
+
+    let userCPF = localStorage.getItem('userId');
+    this.getUser(userCPF);
+
+  }  
+
+   	getUser(cpf) {
+     	this.usersService.getUserByCPF(cpf)
+     	.subscribe(user => this.userData = user);
+  	}
+
+  recoveryPassword(){
+  	this.usersService.recoveryPassword(this.userData.email, this.userData.senha)
+  	.subscribe(() => {
+  	 	alert("O email com sua senha foi enviado!");
+  	});
+  	console.log(this.userData.email);
+  }
+}
